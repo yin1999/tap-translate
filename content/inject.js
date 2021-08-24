31ce72a46f1cb55418c9c66cc99941f47a7e01eb
yin1999 <15844309+yin1999@users.noreply.github.com>
2021-08-24 21:49:28 +0800

    perf: remove the hover element when not showing
    Removing the hover element to keep the dom pure.

--- content/inject.js
+++ content/inject.js
@@ -3,16 +3,9 @@ const innerContent = document.createElement('p')
 innerContent.setAttribute('class', 'popover__message')
 bubbleDOM.appendChild(innerContent)
 bubbleDOM.setAttribute('class', 'popover__content')
-document.body.appendChild(bubbleDOM)
 
-let changed = false
-
-// Lets listen to mouseup DOM events.
+// Listen to mouseup DOM events.
 document.addEventListener('mouseup', e => {
-	if (!changed) {
-		return
-	}
-	changed = false
 	const selection = document.getSelection()
 	const selectedText = selection.toString().trim()
 	if (selectedText.length > 0) {
@@ -32,17 +25,16 @@ document.addEventListener('mouseup', e => {
 	}
 })
 
-document.addEventListener('selectionchange', e => {
-	changed = true
-})
+// Remove the bubble when we click on the screen.
+function removeBubble() {
+	document.removeEventListener('mousedown', removeBubble)
+	document.body.removeChild(bubbleDOM)
+}
 
-// Close the bubble when we click on the screen.
-document.addEventListener('mousedown', () => {
-	bubbleDOM.style.visibility = 'hidden'
-})
-
-// Move that bubble to the appropriate location.
+// Show the bubble to the appropriate location.
 function renderBubble(x, y, selection) {
+	document.body.appendChild(bubbleDOM)
+	document.addEventListener('mousedown', removeBubble)
 	innerContent.innerText = selection
 	bubbleDOM.style.top = y + 'px'
 	bubbleDOM.style.left = x + 'px'
